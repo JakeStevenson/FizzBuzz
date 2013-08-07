5e8fea52522d6a549b092ce99e47bed3f52ae856
Jake Stevenson <jakes@jakes.us>
2013-08-07 13:03:41 -0500

    Added renderer test

---
+++ FizzBuzzTest/TestRenderer.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace FizzBuzzTest
+{
+    [TestFixture]
+    public class TestRenderer
+    {
+        [Test]
+        public void TestDefaultOutput()
+        {
+            var renderer = new FizzBuzzLibrary.FizzBuzzRenderer();
+            Assert.That(renderer.FizzBuzzOutput(3) == "Fizz", "Expected 'Fizz' for 3.");
+            Assert.That(renderer.FizzBuzzOutput(5) == "Buzz", "Expected 'Buzz' for 5.");
+            Assert.That(renderer.FizzBuzzOutput(15) == "FizzBuzz", "Expected 'FizzBuzz' for 15.");
+
+        }
+    }
+}
